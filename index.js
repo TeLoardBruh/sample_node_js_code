c0e7efb284deaf4a5295b708c188ff848c489ac5
Rax <damnloardbruh@gmail.com>
2019-12-15 23:40:14 +0700

    first try

--- index.js
+++ index.js
@@ -37,7 +37,7 @@ app.post('/webhook/', function(req, res) {
 function sendText(sender, text) {
 	let messageData = {text: text}
 	request({
-		url: "https://graph.facebook.com/v2.6/me/messages",
+		url: "https://graph.facebook.com/v5.0/me/messages",
 		qs : {access_token: token},
 		method: "POST",
 		json: {
