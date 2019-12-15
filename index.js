0a8148b845f96fe073de9a87c8c8de82c8505a15
Rax <damnloardbruh@gmail.com>
2019-12-16 00:20:58 +0700

    fff

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ app.post('/webhook/', function (req, res) {
     res.sendStatus(200);
 })
 
-function decideMessage(sender, text) {
+function decideMessage(sender, text1) {
     let text = text1.tolowerCase();
     if (text.includes("dog")) {
         sendImageMessage(sender)
