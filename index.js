bef2ffe58f6b4b16e94d3f7daa8150e602a7a3c0
Rax <damnloardbruh@gmail.com>
2019-12-15 23:34:46 +0700

    first try

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const request = require('request')
 
 
 const app = express()
-PORT = 8080
+let PORT = 8080
 let token = 'EAAjUYnJpZAMoBAOBemtbwdjsZAbg906HrasCLyjpn1xcEg7583VvE6T470ZCLShZBLAGDS25LsstcyxlS248lH2jYzeAZBt09k1Gcuu41JEkv53BKtcKnHRZAcnhUtSSy68tZBi7LsdxQpcfrXSx90QC5i7d7ngKbfTZAogZAjZCXQwQZDZD'
 app.set('port', (process.env.PORT || 5000))
 
