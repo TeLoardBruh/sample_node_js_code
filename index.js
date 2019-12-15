59f83283473bbf2ac1443c548d8fb21f0450abd4
Rax <damnloardbruh@gmail.com>
2019-12-15 22:58:52 +0700

    first try

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const request = require('request')
 
 const app = express()
 PORT = 8080
+token = 'EAAjUYnJpZAMoBAOBemtbwdjsZAbg906HrasCLyjpn1xcEg7583VvE6T470ZCLShZBLAGDS25LsstcyxlS248lH2jYzeAZBt09k1Gcuu41JEkv53BKtcKnHRZAcnhUtSSy68tZBi7LsdxQpcfrXSx90QC5i7d7ngKbfTZAogZAjZCXQwQZDZD'
 app.set('port', (process.env.PORT || 5000))
 
 // Allow us to process the data 
@@ -27,6 +28,38 @@ app.get('/webhook/', function (req, res) {
     res.send("wrong token")
 })
 
+app.get('/webhook/', function (req, res) {
+    let messaging_event = req.body.entry[0].messaging_event;
+    for (let i = 0 ; i < messaging_event.length; i++){
+        let event = messaging_event[i];
+        let sender = event.sender.id
+        if(event.message && event.message.text){
+            let text = event.message.text;
+            sendText(sender, "Text Echo : " + text.substring(0,100))
+        }
+    }
+    res.sendStatus(200);
+})
+
+function sendText(sender, text){
+    let messageData = {text : text};
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {access_token, token},
+        method: 'POST',
+        json: {
+            receipt: {id:sender},
+            message: messageData
+        }
+    }), function(error, req,res){
+        if(error){
+            console.log("Sending is Error");
+        }
+        else if(req.body.error){
+            console.log("responding is Error");
+        }
+    }
+}
 app.listen(app.get('port'), function(){
     console.log(`listenning to ${PORT}`);
 })
