f094b33b1ae33304ab1ee71760f483bbc631c165
Rax <damnloardbruh@gmail.com>
2019-12-16 16:26:32 +0700

    ff

--- index.js
+++ index.js
@@ -1,159 +1,162 @@
-'use strict'
+"use strict";
 
-const express = require('express')
-const bodyParser = require('body-parser')
-const request = require('request')
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
-const app = express()
-let token = 'EAAjUYnJpZAMoBAOBemtbwdjsZAbg906HrasCLyjpn1xcEg7583VvE6T470ZCLShZBLAGDS25LsstcyxlS248lH2jYzeAZBt09k1Gcuu41JEkv53BKtcKnHRZAcnhUtSSy68tZBi7LsdxQpcfrXSx90QC5i7d7ngKbfTZAogZAjZCXQwQZDZD'
-app.set('port', (process.env.PORT || 5000))
+const app = express();
+let token =
+  "EAAjUYnJpZAMoBAOBemtbwdjsZAbg906HrasCLyjpn1xcEg7583VvE6T470ZCLShZBLAGDS25LsstcyxlS248lH2jYzeAZBt09k1Gcuu41JEkv53BKtcKnHRZAcnhUtSSy68tZBi7LsdxQpcfrXSx90QC5i7d7ngKbfTZAogZAjZCXQwQZDZD";
+app.set("port", process.env.PORT || 5000);
 
-// Allow us to process the data 
-app.use(bodyParser.urlencoded({
+// Allow us to process the data
+app.use(
+  bodyParser.urlencoded({
     extended: false
-}))
-app.use(bodyParser.json())
+  })
+);
+app.use(bodyParser.json());
 
-// Routes 
+// Routes
 
-app.get('/', function (req, res) {
-    res.send("Hi this is my node ChatBot")
-})
+app.get("/", function(req, res) {
+  res.send("Hi this is my node ChatBot");
+});
 
 // Secure Stuff
-app.get('/webhook/', function (req, res) {
-    if (req.query['hub.verify_token'] === "raxcyRax") {
-        res.send(req.query['hub.challenge'])
-    }
-    res.send("wrong token")
-})
+app.get("/webhook/", function(req, res) {
+  if (req.query["hub.verify_token"] === "raxcyRax") {
+    res.send(req.query["hub.challenge"]);
+  }
+  res.send("wrong token");
+});
 
-app.post('/webhook/', function (req, res) {
-    let messaging_event = req.body.entry[0].messaging;
-    for (let i = 0; i < messaging_event.length; i++) {
-        let event = messaging_event[i];
-        let sender = event.sender.id
-        if (event.message && event.message.text) {
-            let text = event.message.text;
-            // sendText(sender, "Text Echo : " + text.substring(0,100))
-            decideMessage(sender, text)
-        }
-        if (event.postback) {
-            let text = JSON.stringify(event.postback)
-            decideMessage(sender, text)
-            continue
-        }
+app.post("/webhook/", function(req, res) {
+  let messaging_event = req.body.entry[0].messaging;
+  for (let i = 0; i < messaging_event.length; i++) {
+    let event = messaging_event[i];
+    let sender = event.sender.id;
+    if (event.message && event.message.text) {
+      let text = event.message.text;
+      // sendText(sender, "Text Echo : " + text.substring(0,100))
+      decideMessage(sender, text);
     }
-    res.sendStatus(200);
-})
+    if (event.postback) {
+      let text = JSON.stringify(event.postback);
+      decideMessage(sender, text);
+      continue;
+    }
+  }
+  res.sendStatus(200);
+});
 
 function decideMessage(sender, text1) {
-    let text = text1.toString().toLowerCase();
-    if (text.includes("hi")) {
-        sendGreeting(sender)
-    }
-    else if (text.includes("dog")){
-        sendImageMessage(sender)
-    }
-    else if (text.includes("cat")) {
-        sendImageMessage(sender)
-    } else {
-        sendText(sender, "...")
-        sendButton(sender, "what is your fav pet ?")
-    }
+  let text = text1.toString().toLowerCase();
+  if (text.includes("hi")) {
+    sendImageMessage(sender);
+  } else if (text.includes("dog")) {
+    sendImageMessage(sender);
+  } else if (text.includes("cat")) {
+    sendImageMessage(sender);
+  } else {
+    sendText(sender, "...");
+    sendButton(sender, "what is your fav pet ?");
+  }
 }
-function sendGreeting (sender,text){
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": text,
-                "buttons": [{
-                        "type": "postback",
-                        "title": "dog",
-                        "payload": "dog"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "cat",
-                        "payload": "cat"
-                    },
-
-                ]
-            }
-        }
+function sendGreeting(sender, text) {
+  let messageData = {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "button",
+        text: text,
+        buttons: [
+          {
+            type: "postback",
+            title: "dog",
+            payload: "dog"
+          },
+          {
+            type: "postback",
+            title: "cat",
+            payload: "cat"
+          }
+        ]
+      }
     }
-    sendRequest(sender, messageData)
+  };
+  sendRequest(sender, messageData);
 }
 
 function sendButton(sender, text) {
-    let messageData = {
-        "attachment": {
-            "type": "template",
-            "payload": {
-                "template_type": "button",
-                "text": text,
-                "buttons": [{
-                        "type": "postback",
-                        "title": "dog",
-                        "payload": "dog"
-                    },
-                    {
-                        "type": "postback",
-                        "title": "cat",
-                        "payload": "cat"
-                    },
-
-                ]
-            }
-        }
+  let messageData = {
+    attachment: {
+      type: "template",
+      payload: {
+        template_type: "button",
+        text: text,
+        buttons: [
+          {
+            type: "postback",
+            title: "dog",
+            payload: "dog"
+          },
+          {
+            type: "postback",
+            title: "cat",
+            payload: "cat"
+          }
+        ]
+      }
     }
-    sendRequest(sender, messageData)
-
+  };
+  sendRequest(sender, messageData);
 }
 
 function sendImageMessage(sender) {
-    let messageData = {
-        "attachment": {
-            "type": "image",
-            "payload": {
-                "url": "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg"
-            }
-        }
+  let messageData = {
+    attachment: {
+      type: "image",
+      payload: {
+        url:
+          "https://cdn.pixabay.com/photo/2016/12/13/05/15/puppy-1903313__340.jpg"
+      }
     }
-    sendRequest(sender, messageData)
+  };
+  sendRequest(sender, messageData);
 }
 
-
 function sendRequest(sender, messageData) {
-    request({
-        url: 'https://graph.facebook.com/v5.0/me/messages',
-        qs: {
-            access_token: token
+  request(
+    {
+      url: "https://graph.facebook.com/v5.0/me/messages",
+      qs: {
+        access_token: token
+      },
+      method: "POST",
+      json: {
+        recipient: {
+          id: sender
         },
-        method: 'POST',
-        json: {
-            recipient: {
-                id: sender
-            },
-            message: messageData
-        }
-    }, function (error, req, res) {
-        if (error) {
-            console.log("Sending is Error");
-        } else if (req.body.error) {
-            console.log("responding is Error");
-        }
-    })
+        message: messageData
+      }
+    },
+    function(error, req, res) {
+      if (error) {
+        console.log("Sending is Error");
+      } else if (req.body.error) {
+        console.log("responding is Error");
+      }
+    }
+  );
 }
 
 function sendText(sender, text) {
-    let messageData = {
-        text: text
-    }
-    sendRequest(sender, messageData)
+  let messageData = {
+    text: text
+  };
+  sendRequest(sender, messageData);
 }
-app.listen(app.get('port'), function () {
-    console.log(`running : port `);
-})
+app.listen(app.get("port"), function() {
+  console.log(`running : port `);
+});
