b6ebbcdde7a565d9a05317e8d05e63567fc089db
Rax <damnloardbruh@gmail.com>
2019-12-20 00:01:23 +0700

    fff

--- index.js
+++ index.js
@@ -73,7 +73,18 @@ function decideMessage(sender, text1) {
     sendGreeting_quick_reply(sender);
   } else if (text.includes("start chat")) {
     start_chating(sender);
-  } else if (text.includes("shop here")) {
+  }
+  else if (text.includes("phone number")) {
+    sendText(sender, "May we have your number ? ");
+  }
+  else if (text.includes("email")) {
+    sendText(sender, "May we have your email ? ");
+  }
+  
+  
+  
+  
+  else if (text.includes("shop here")) {
     sendMessageGenericShopHere(sender);
   } else if (text.includes("check price")) {
     sendList(sender);
