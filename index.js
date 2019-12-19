ecb4432925206d781aa2bfa48f725977bf65d6c0
Rax <damnloardbruh@gmail.com>
2019-12-19 23:27:30 +0700

    phone number

--- index.js
+++ index.js
@@ -78,11 +78,11 @@ function decideMessage(sender, text1) {
   } else if (text.includes("check price")) {
     sendList(sender);
   } else if (text.includes("Go back")) {
-    // sendGreeting_quick_reply(sender);
+    sendGreeting_quick_reply(sender);
   } else {
     // sendText(sender, "Hello welcome to my service");
     // sendButton(sender, "what is your fav pet ?");
-    sendGreeting_quick_reply(sender);
+    // sendGreeting_quick_reply(sender);
   }
 }
 // =====================================================================================================================================
